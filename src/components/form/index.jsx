3701f99fbc5299eea69fbc2fb8cffa0887dbfbde
Raul Souza <xlraullx9@hotmail.com>
2025-08-23 17:50:50 -0300

    Adicionado opção de remover imagem de avatar

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react';
 import './index.css'
 import { useForm } from 'react-hook-form'
 
 const Form = () => {
-    const { register, handleSubmit, formState: { errors }, watch } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const [avatarUrl, setAvatarUrl] = useState();
+
+    useEffect(() => {
+        return () => {
+            if (avatarUrl) {
+                URL.revokeObjectURL(avatarUrl);
+            }
+        };
+    }, [avatarUrl]);
+
+    function handleAvatarChange(e) {
+        const file = e.target.files[0];
+        if (file) {
+            setAvatarUrl(URL.createObjectURL(file));
+        }
+    }
+
+    function removeImage() {
+        setAvatarUrl(null)
+    } 
 
     const handleSubmitForm = (data) => {
         console.log(data)
@@ -10,29 +32,38 @@ const Form = () => {
 
     function validateImage(fileList) {
         const image = fileList[0];
-        if (!image || image.type == "" || image.size == 0 ) return "Upload your photo (JPG or PNG, max size: 500KB).";
+        if (!image || image.type == "" || image.size == 0 || avatarUrl == null) return "Upload your photo (JPG or PNG, max size: 500KB).";
         if (image.size > 500 * 1024) return "File too large. Please upload a photo under 500KB";
         return true;
     }
-    
-    function isString(value) { 
-        if (typeof value !== 'string') return 'Invalid'
-        
-    } 
+
+    function isString(value) {
+        if (typeof value !== 'string' || value.trim() === '') return 'Invalid';
+        return true;
+    }
 
     return (
         <form className="form" id="ticketForm" onSubmit={handleSubmit(handleSubmitForm)}>
             <div className="upload-avatar">
                 <p>Upload Avatar</p>
                 <div className="upload-area">
-                    <input 
-                    type="file" 
-                    id="avatar"  
-                    accept="image/png, image/jpeg"
-                    {...register('avatar', 
-                        {validate: validateImage }
-                    )} />
-                    <label htmlFor="avatar">Drag and drop or click to upload</label>
+                    <input
+                        type="file"
+                        id="avatar"
+                        accept="image/png, image/jpeg"
+                        {...register('avatar',
+                            { validate: validateImage }
+                        )} 
+                        onChange={handleAvatarChange}/>
+                    
+                    <label htmlFor="avatar"> 
+                        <img className={avatarUrl ? 'avatar-image' : null} src={avatarUrl} />  
+                        <span>Drag and drop or click to upload</span>
+                    </label>
+                    <div className={avatarUrl ? 'avatar-buttons' : ''}>
+                        <button type="button" onClick={removeImage}><span>Remove image</span></button>
+                        <button type="button"> <label htmlFor="avatar"><span>Change image</span></label></button>
+                    </div>
                 </div>
                 <div className="upload-info">
                     {errors.avatar ? <p className="error-text">{errors.avatar.message}</p> : <p>Upload your photo (JPG or PNG, max size: 500KB).</p>}
@@ -83,4 +114,4 @@ const Form = () => {
     )
 }
 
-export default Form 
+export default Form
