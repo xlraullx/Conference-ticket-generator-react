3d44c316b164a55172e14004a0ab18d2c88a6e6c
Raul Souza <xlraullx9@hotmail.com>
2026-01-13 12:03:56 -0300

    refactor: melhora validação de tamanho da imagem

--- src/components/form/functions.js
+++ src/components/form/functions.js
@@ -12,7 +12,7 @@ function removeImage(setAvatarUrl, setValue) {
 
 function validateImage(fileList) {
     const image = fileList[0];
-    if (!image || image.type == "" || image.size == 0) return "Upload your photo (JPG or PNG, max size: 500KB).";
+    if (!image || image.type === "" || image.size === 0) return "Upload your photo (JPG or PNG, max size: 500KB).";
     if (image.size > 500 * 1024) return "File too large. Please upload a photo under 500KB";
     return true;
 }
